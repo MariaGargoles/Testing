4d556468cc51ebf366d66b8056b441da33127293
Maria Gargoles <mariagargoles@live.com>
2024-07-09 19:53:07 +0200

    ADD: finish jest test

--- index.test.js
+++ index.test.js
@@ -1,29 +1,157 @@
-const { suma, Room, Booking } = require("./suma");
+const { Room, Booking } = require("./suma");
 
-test("sumar 1 + 2 es igual a 3, pero queremos probar que el test puede fallar asi que indicamos 4", () => {
-  expect(suma(1, 2)).toBe(4);
+const habitacion1 = new Room({
+  name: "Suite A-107",
+  bookings: [],
+  rate: 40000,
+  discount: 50,
 });
 
-test("isOccupied devuelve true si la habitación está ocupada en una fecha específica", () => {
-  const room = new Room({
-    name: "Room 1",
-    bookings: [],
-    rate: 10000,
-    discount: 8,
-  });
+const reserva1Habitacion1 = new Booking({
+  name: "Juan Pérez",
+  email: "juan.perez@example.com",
+  checkin: "2024-06-08",
+  checkout: "2024-06-09",
+  discount: 10,
+  room: habitacion1,
+});
+habitacion1.addBooking(reserva1Habitacion1);
+const reserva2Habitacion1 = new Booking({
+  name: "Ana Gómez",
+  email: "ana.gomez@example.com",
+  checkin: "2024-06-05",
+  checkout: "2024-06-06",
+  discount: 0,
+  room: habitacion1,
+});
+habitacion1.addBooking(reserva2Habitacion1);
+const reserva3Habitacion1 = new Booking({
+  name: "Pedro Fernández",
+  email: "pedro.fernandez@example.com",
+  checkin: "2024-06-10",
+  checkout: "2024-06-12",
+  discount: 20,
+  room: habitacion1,
+});
+habitacion1.addBooking(reserva3Habitacion1);
 
-  const booking = new Booking({
-    name: "Jane Doe",
-    email: "janedoe@gmail.com",
-    checkin: "2024-05-01",
-    checkout: "2024-05-03",
-    discount: 0,
-    room: room,
-  });
+const habitacion2 = new Room({
+  name: "Doble B-202",
+  bookings: [],
+  rate: 20000,
+  discount: 20,
+});
 
-  room.addBooking(booking);
+const reserva1Habitacion2 = new Booking({
+  name: "Laura Martínez",
+  email: "laura.martinez@example.com",
+  checkin: "2024-06-07",
+  checkout: "2024-06-10",
+  discount: 10,
+  room: habitacion2,
+});
+habitacion2.addBooking(reserva1Habitacion2);
+const reserva2Habitacion2 = new Booking({
+  name: "Ana Gómez",
+  email: "ana.gomez@example.com",
+  checkin: "2024-06-05",
+  checkout: "2024-06-06",
+  discount: 0,
+  room: habitacion2,
+});
+habitacion2.addBooking(reserva2Habitacion2);
+const reserva3Habitacion2 = new Booking({
+  name: "Pedro Fernández",
+  email: "pedro.fernandez@example.com",
+  checkin: "2024-06-10",
+  checkout: "2024-06-13",
+  discount: 20,
+  room: habitacion2,
+});
+habitacion2.addBooking(reserva3Habitacion2);
 
-  expect(room.isOccupied("2024-05-01")).toBe(true);
-  expect(room.isOccupied("2024-05-03")).toBe(true);
-  expect(room.isOccupied("2024-05-11")).toBe(false);
+test("debe verificar que la habitación 1 está ocupada en la fecha 2024-06-08", () => {
+  expect(habitacion1.isOccupied("2024-06-08")).toBe(true);
+});
+
+test("debe verificar que la habitación 1 no está ocupada en la fecha 2024-06-12", () => {
+  expect(habitacion1.isOccupied("2024-06-12")).toBe(false);
+});
+
+test("debe verificar que la habitación 1 no está ocupada en la fecha 2024-06-09", () => {
+  expect(habitacion1.isOccupied("2024-06-09")).toBe(false);
+});
+
+test("debe verificar que la habitación 1 no está ocupada en la fecha 2024-06-07", () => {
+  expect(habitacion1.isOccupied("2024-06-07")).toBe(false);
+});
+
+test("debe verificar que la habitación 1 no está ocupada en la fecha 2024-06-12", () => {
+  expect(habitacion1.isOccupied("2024-06-12")).toBe(false);
+});
+
+test("debe devolver que la habitación 1 tiene un 50% de ocupación entre el 2024-06-08 y el 2024-06-09", () => {
+  expect(habitacion1.occupancyPercetage("2024-06-08", "2024-06-09")).toBe(50);
+});
+
+test("debe devolver que la habitación 1 tiene un 0% de ocupación entre el 2024-06-13 y el 2024-06-20", () => {
+  expect(habitacion1.occupancyPercetage("2024-06-13", "2024-06-20")).toBe(0);
+});
+
+test("debe devolver que la habitación 1 tiene un 100% de ocupación entre el 2024-06-10 y el 2024-06-11", () => {
+  expect(habitacion1.occupancyPercetage("2024-06-10", "2024-06-11")).toBe(100);
+});
+
+test("debe devolver que con ambas habitaciones la ocupación es del 0% entre el 2024-06-13 y el 2024-06-20", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(
+    Room.totalOccupancyPercetage(habitaciones, "2024-06-13", "2024-06-20")
+  ).toBe(0);
+});
+
+test("debe devolver que con ambas habitaciones la ocupación es del 100% entre el 2024-06-10 y el 2024-06-11", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(
+    Room.totalOccupancyPercetage(habitaciones, "2024-06-10", "2024-06-11")
+  ).toBe(100);
+});
+
+test("debe devolver que con ambas habitaciones la ocupación es del 50% entre el 2024-06-05 y el 2024-06-06", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(
+    Room.totalOccupancyPercetage(habitaciones, "2024-06-05", "2024-06-06")
+  ).toBe(50);
+});
+
+test("debe devolver ambas habitaciones entre el 2024-06-13 y el 2024-06-20", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(Room.availableRooms(habitaciones, "2024-06-13", "2024-06-20")).toEqual(
+    [habitacion1, habitacion2]
+  );
+});
+
+test("debe devolver un array vacío entre el 2024-06-10 y el 2024-06-11", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(Room.availableRooms(habitaciones, "2024-06-10", "2024-06-11")).toEqual(
+    []
+  );
+});
+
+test("debe devolver un array con solo la habitación 1 entre el 2024-06-12 y el 2024-06-14", () => {
+  const habitaciones = [habitacion1, habitacion2];
+  expect(Room.availableRooms(habitaciones, "2024-06-12", "2024-06-14")).toEqual(
+    [habitacion1]
+  );
+});
+
+test("debe devolver 18000 como tarifa total para la reserva 1 de la habitación 1", () => {
+  expect(reserva1Habitacion1.getFee()).toBe(18000);
+});
+
+test("debe devolver 20000 como tarifa total para la reserva 2 de la habitación 1", () => {
+  expect(reserva2Habitacion1.getFee()).toBe(20000);
+});
+
+test("debe devolver 32000 como tarifa total para la reserva 3 de la habitación 1", () => {
+  expect(reserva3Habitacion1.getFee()).toBe(32000);
 });
--- suma.js
+++ suma.js
@@ -11,12 +11,49 @@ class Room {
   };
 
   isOccupied = (date) => {
-    return this._bookings.some((booking) => {
-      const checkin = new Date(booking._checkin);
-      const checkout = new Date(booking._checkout);
-      const targetDate = new Date(date);
-      return targetDate >= checkin && targetDate <= checkout;
+    let isOccupied = false;
+    this._bookings.forEach((booking) => {
+      if (booking._checkin <= date && date < booking._checkout) {
+        isOccupied = true;
+        return;
+      }
     });
+    return isOccupied;
+  };
+
+  occupancyPercetage = (startDate, endDate) => {
+    const start = Date.parse(startDate);
+    const end = Date.parse(endDate);
+    let days = 0;
+    let occupiedDays = 0;
+    for (let i = start; i <= end; i += 86400000) {
+      days++;
+      const date = new Date(i);
+      let year = date.getFullYear();
+      let month = (date.getMonth() + 1).toString().padStart(2, "0");
+      let day = date.getDate().toString().padStart(2, "0");
+      if (this.isOccupied(`${year}-${month}-${day}`)) occupiedDays++;
+    }
+    return (occupiedDays / days) * 100;
+  };
+
+  static totalOccupancyPercetage = (rooms, startDate, endDate) => {
+    let percentage = 0;
+    rooms.forEach((room) => {
+      percentage += room.occupancyPercetage(startDate, endDate);
+    });
+
+    return percentage / rooms.length;
+  };
+
+  static availableRooms = (rooms, startDate, endDate) => {
+    let roomArray = [];
+    rooms.forEach((room) => {
+      if (room.occupancyPercetage(startDate, endDate) === 0)
+        roomArray.push(room);
+    });
+
+    return roomArray;
   };
 }
 
@@ -29,10 +66,16 @@ class Booking {
     this._discount = discount;
     this._room = room;
   }
+
+  getFee = () => {
+    const start = Date.parse(this._checkin);
+    const end = Date.parse(this._checkout);
+    const nights = (end - start) / 86400000;
+    const roomPrice =
+      this._room._rate * nights -
+      (this._room._rate * nights * this._room._discount) / 100;
+    return roomPrice - roomPrice * (this._discount / 100);
+  };
 }
 
-function suma(a, b) {
-  return a + b;
-}
-
-module.exports = { suma, Room, Booking };
+module.exports = { Room, Booking };
