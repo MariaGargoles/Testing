c33177e6b0041635558d865ed117702afdcdb7ef
Maria Gargoles <mariagargoles@live.com>
2024-07-09 17:45:49 +0200

    ADD: room and 1 booking to test

--- suma.js
+++ suma.js
@@ -1,5 +1,23 @@
-function suma(a, b) {
-  return a + b;
+class Room {
+  constructor({ name, bookings, rate, discount }) {
+    this._name = name;
+    this._bookings = bookings;
+    this._rate = rate;
+    this._discount = discount;
+  }
+
+  addBooking = (booking) => {
+    this._bookings.push(booking);
+  };
 }
 
-module.exports = suma;
+const Booking = new Booking({
+  name: "Lorem ipsun",
+  email: "Loremipsun@gmail.com",
+  checkin: "2024-05-20",
+  checkout: "2024-05-23",
+  discount: 15,
+  room: RoomExample1,
+});
+
+module.exports = { suma, Room, Booking };
